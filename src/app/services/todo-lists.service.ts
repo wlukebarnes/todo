a9c571ef87bd4d539a830cafa10a9eb4c8b5609c
wlukebarnes <william.barnes4@t-mobile.com>
2020-06-11 11:43:12 -0700

    17 done

--- src/app/services/todo-lists.service.ts
+++ src/app/services/todo-lists.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import { StorageService } from './storage.service'
+
+const todoListStorageKey = 'Todo_List';
+
+const defaultTodoList = [
+  {title: 'install NodeJS'},
+  {title: 'install Angular CLI'},
+  {title: 'create new app'},
+  {title: 'serve app'},
+  {title: 'develop app'},
+  {title: 'deploy app'},
+];
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListsService {
-  private todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
-  constructor() { }
+  private todoList: TodoItem[]
+  constructor(private storageService: StorageService) {
+    this.todoList = storageService.getData(todoListStorageKey) || defaultTodoList;
+   }
 
   getTodoList() {
     return this.todoList;
@@ -21,5 +28,24 @@ export class TodoListsService {
 
   addItem(item: TodoItem) {
     this.todoList.push(item);
+    this.saveList();
+  }
+
+  updateItem(item: TodoItem, changes) {
+    this.todoList[this.getIndex(item)] = {...item,...changes};
+    this.saveList();
+  }
+
+  private saveList() {
+    this.storageService.setData(todoListStorageKey, this.todoList);
+  }
+
+  deleteItem(item: TodoItem) {
+    this.todoList.splice(this.getIndex(item),1);
+    this.saveList();
+  }
+
+  private getIndex(item: TodoItem) {
+    return this.todoList.indexOf(item);
   }
 }
